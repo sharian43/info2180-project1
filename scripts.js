092f5ada7f6e5d21cec4e49bf568ce74bc65c854
sharian43 <shariant98@gmail.com>
2023-10-28 15:43:04 -0500

    A sprinkle of JS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function() {
+    const submitButton = document.querySelector('button');
+    const message = document.querySelector('.message');
+    
+    submitButton.type = 'button'; // Prevents page reload on submit button click
+
+    submitButton.addEventListener('click', function() {
+        const emailInput = document.querySelector('input');
+        const email = emailInput.value.trim();
+        
+        if (email.includes('@') && email.includes('.')) {
+            message.innerHTML = `Thank you! Your email ${email} has been added to our mailing list!`;
+        } else {
+            message.innerHTML = 'Error: Please enter a valid email address.';
+        }
+    });
+});
